7de5314d9d7dd52958627777151f2bdce6c70fc3
gmacfingertip <colm@colms-mbp.home>
2015-01-20 00:31:11 +0000

    fireonapp
    video repo

---
+++ plugin.video.fireon/addon.py
@@ -0,0 +1,13 @@
+import sys
+import xbmcgui
+import xbmcplugin
+
+addon_handle = int(sys.argv[1])
+
+xbmcplugin.setContent(addon_handle, 'movies')
+
+url = 'http://file.xbmcmxtv.com/happy.m3u'
+li = xbmcgui.ListItem('fireon!', iconImage='DefaultVideo.png')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+xbmcplugin.endOfDirectory(addon_handle)
